9950efe1a38c1d5e29db92b032f2b980f912d3b8
mkashirin <m.k4shirin@yandex.ru>
2024-06-29 15:53:56 +0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -30,15 +30,23 @@ impl<'a> Config<'a> {
             return Err("Not enough arguments");
         }
 
-        if let Ok(parsed_bool) = bool::from_str(&args[3]) {
-            Ok(Self {
-                query: &args[1],
-                file_name: &args[2],
-                case_sensitive: parsed_bool,
-            })
-        } else {
-            Err("Failed to parse bool")
-        }
+        let parsed_bool = bool::from_str(&args[3]).map_err(|_| "Failed to parse bool")?;
+
+        Ok(Self {
+            query: &args[1],
+            file_name: &args[2],
+            case_sensitive: parsed_bool,
+        })
+
+        // if let Ok(parsed_bool) = bool::from_str(&args[3]) {
+        //     Ok(Self {
+        //         query: &args[1],
+        //         file_name: &args[2],
+        //         case_sensitive: parsed_bool,
+        //     })
+        // } else {
+        //     Err("Failed to parse bool")
+        // }
     }
 }
 
